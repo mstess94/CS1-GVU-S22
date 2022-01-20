1fcb9de9c6b6923d2748d3b4c17a9ba40a61d171
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-01-19 22:30:18 -0600

    Create randomwalk.py shell

---
+++ Jan-20/randomwalk.py
@@ -0,0 +1,16 @@
+"""
+Draw path of a random walk. 
+
+Created on Wed Jan 19 22:28:25 2022
+@author: mrobbins
+"""
+
+from turtle import * 
+
+def random_move():
+    """Take random step on a grid."""
+    
+def main():
+    print("Canary")
+    
+main()
