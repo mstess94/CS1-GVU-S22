bb7697fe68e46bd6413e00fa8d49a417e3f1b029
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-01-19 22:27:13 -0600

    Create shell of spiral.py

---
+++ Jan-20/spiral.py
@@ -0,0 +1,14 @@
+"""
+Created on Wednesday, Jan 19 22:25:25 2022
+@author: mrobbins
+"""
+
+from turtle import * 
+
+def spiral():
+  print("Canary")
+  
+def main():
+  print("Canary main")
+  
+main()
