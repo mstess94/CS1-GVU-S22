f52e9062331abc23d430ce923a2c4289e1bff4fd
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-01-24 22:20:51 -0600

    Create mymax2.py shell

---
+++ Jan-25/mymax2.py
@@ -0,0 +1,11 @@
+# mymax2.py
+# Write max function without build-in max()
+
+def mymax2(x, y):
+    """ Return larger of x and y. """
+    print("Canary")
+    
+def main():
+    print("MyMax: Enter two values to find the larger.")
+    
+main()
