ab32564310b72c99ddcd5092b711e5fe7ba6eaac
Melissa Robbins <mrobbins@grandview.edu>
2022-01-27 18:29:14 -0600

    add in class files

--- Jan-20/grades.py
+++ Jan-20/grades.py
@@ -1,16 +1,29 @@
 """
-Create a program that prints your letter grade based on your numeric grade
+Create a program that prints your letter grade based on 
+your numeric grade
 
 Created on Tue Jan 18 17:52:25 2022
 @author: mrobbins
 """
 
-from turtle import * 
-
-def letter_grade():
+def letter_grade(grade):
     """Print the letter grade given the numeric grade"""
+    if grade >= 91:
+        print("Excellent")
+    elif grade >= 81:
+        print("b")
+    elif grade >= 70:
+        print("Cool kids")
+    elif grade >= 61:
+        print("Doing great!")
+    else:
+        print("Fantastic!")
     
 def main():
-    print("Canary")
+#    letter_grade(95)
+#    letter_grade(87)
+#    letter_grade(78)
+#    letter_grade(63)
+    letter_grade(90)
     
 main()
--- Jan-20/randomwalk.py
+++ Jan-20/randomwalk.py
@@ -6,11 +6,23 @@ Created on Wed Jan 19 22:28:25 2022
 """
 
 from turtle import * 
+from random import randrange
 
-def random_move():
+def random_move(distance):
     """Take random step on a grid."""
+    left(randrange(0, 360, 90))
+    forward(distance)
     
 def main():
-    print("Canary")
+    speed(0)
+    while abs(xcor()) < 200 and abs(ycor()) < 200:
+        random_move(25)
+    exitonclick()
     
 main()
+
+
+
+
+
+
