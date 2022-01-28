a52c7f21e94c4aed1c844def3dbcead5c75d495c
Melissa Robbins <mrobbins@grandview.edu>
2022-01-27 19:22:52 -0600

    added files for jan 25 and 27

--- Jan-25/mymax2.py
+++ Jan-25/mymax2.py
@@ -3,9 +3,18 @@
 
 def mymax2(x, y):
     """ Return larger of x and y. """
-    print("Canary")
+    largest_so_far = x
+    if y > largest_so_far:
+        largest_so_far = y
+        
+    return largest_so_far
     
 def main():
     print("MyMax: Enter two values to find the larger.")
+    first = float(input("First value: "))
+    second = float(input("Second value: "))
+    
+    larger = mymax2(first, second)
+    print("The larger value is: ", larger)
     
 main()
