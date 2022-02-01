830e28f2c163e70a8764e2e2448ce7aa5cd29f01
MRobbins-GVU <97652502+MRobbins-GVU@users.noreply.github.com>
2022-02-01 00:31:16 -0600

    Add shell of guess.py

---
+++ Feb-1/guess.py
@@ -0,0 +1,13 @@
+# guess.py
+# User guesses a random number
+
+from random import randint
+
+def userguess(secret): 
+    """ Ask user for guesses until matching secret. """
+    
+    
+def main():
+    print("Canary")
+    
+main()
